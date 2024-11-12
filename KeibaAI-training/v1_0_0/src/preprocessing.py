68d3bcc89727dd15561fe4ba07278a5b346ec5bc
hisafumi <octn.wps.0108@gmail.com>
2024-11-12 12:06:48 +0900

    レース情報のスクレイピングと特徴量生成クラスを追加

--- KeibaAI-training/v1_0_0/src/preprocessing.py
+++ KeibaAI-training/v1_0_0/src/preprocessing.py
@@ -41,6 +41,10 @@ race_class_mapping = {
     "1000万以下": 3,
     "1600万以下": 4
     }
+around_mapping = {
+    "右":0,
+    "左":1
+}
 
 def process_results(
     input_dir: Path = INPUT_DIR,
@@ -157,24 +161,51 @@ def process_race_info(
     race_type_mapping: dict = race_type_mapping,
     weather_mapping: dict = weather_mapping,
     ground_state_mapping: dict = ground_state_mapping,
-    race_class_mapping: dict = race_class_mapping
+    race_class_mapping: dict = race_class_mapping,
+    around_mapping: dict = around_mapping
 ) -> pd.DataFrame:
     """
     レース情報テーブルのrawデータファイルをinput_dirから読み込んで、加工し、
     output_dirに保存する関数。
     """
     df = pd.read_csv(input_dir / save_filename, sep="\t")
-    df["rank"] = pd.to_numeric(df["着順"], errors="coerce")
-    df.dropna(subset=["rank"], inplace=True)
-    df["rank"] = df["rank"].astype(int)
-    df["date"] = pd.to_datetime(df["日付"])
-    df["weather"] = df["天気"].map(weather_mapping)
-    # df["weather"] = df["weather"].astype(int)
-    df["race_type"] = df["距離"].str[0].map(race_type_mapping)
-    df["course_len"] = df["距離"].str.extract(r"(\d+)").astype(int)
-    df["ground_state"] = df["馬場"].map(ground_state_mapping)
-    df["rank_diff"] = df["着差"].map(lambda x: 0 if x < 0 else x)
-    df["prize"] = df["賞金"].fillna(0)
-    regex_race_class = "|".join(race_class_mapping.keys())
-    df["race_class"] = df["レース名"].str.extract(rf"({regex_race_class})")[0].map(race_class_mapping)
-    df.rename(columns={"頭数": "n_horses"}, inplace=True)
+    # evalで文字列型の列をリスト型に変換し、一時的な列を作成
+    df["tmp"] = df["info1"].map(lambda x: eval(x)[0])
+    # ダートor芝or障害
+    df["race_type"] = df["tmp"].str[0].map(race_type_mapping)
+    # 右or左or直線
+    df["around"] = df["tmp"].str[1].map(around_mapping)
+    df["course_len"] = df["tmp"].str.extract(r"(\d+)")
+    df["weather"] = df["info1"].str.extract(r"天候:(\w+)")[0].map(weather_mapping)
+    df["ground_state"] = (
+        df["info1"].str.extract(r"(芝|ダート|障害):(\w+)")[1].map(ground_state_mapping)
+    )
+    df["date"] = pd.to_datetime(
+        df["info2"].map(lambda x: eval(x)[0]), format="%Y年%m月%d日"
+    )
+    #'新馬|未勝利|1勝クラス|2勝クラス|3勝クラス|オープン|G3|G2|G1|特別|500万下|1000万下|1600万下|OP'
+    regex_race_class = "|".join(race_class_mapping)
+    df["race_class"] = (
+        df["title"]
+        .str.extract(rf"({regex_race_class})")
+        # タイトルからレース階級情報が取れない場合はinfo2から取得
+        .fillna(df["info2"].str.extract(rf"({regex_race_class})"))[0]
+        .map(race_class_mapping)
+    ) 
+    df["place"] = df["race_id"].astype(str).str[4:6].astype(int)
+
+    df = df[
+        [
+            "race_id",
+            "date",
+            "race_type",
+            "around",
+            "course_len",
+            "weather",
+            "ground_state",
+            "race_class",
+            "place"
+        ]
+    ]
+    df.to_csv(output_dir / save_filename, sep="\t", index=False)
+    return df
